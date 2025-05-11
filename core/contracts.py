4e591de3bf9e4dbe90a8bf66040dee1b599994fe
ismailtester <ismailtester@yandex.ru>
2025-05-11 12:10:59 +0300

    Добавлены тесты: 1. Получение списка пользователей 2. Получение информации об одном пользователе 3. Попытка получить информацию о несуществующем пользователе 4. Получение списка ресурсов 5. Получение информации об одном ресурсе 6. Попытка получить информацию о несуществующем ресурсе

---
+++ core/contracts.py
@@ -0,0 +1,45 @@
+USER_DATA_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "id": {
+            "type": "integer"
+        },
+        "email": {
+            "type": "string"
+        },
+        "first_name": {
+            "type": "string"
+        },
+        "last_name": {
+            "type": "string"
+        },
+        "avatar": {
+            "type": "string"
+        }
+    },
+    "required": ["id", "email", "first_name", "last_name", "avatar"],
+    "additionalProperties": False
+}
+
+RESOURCE_DATA_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "id": {
+            "type": "integer"
+        },
+        "name": {
+            "type": "string"
+        },
+        "year": {
+            "type": "integer"
+        },
+        "color": {
+            "type": "string"
+        },
+        "pantone_value": {
+            "type": "string"
+        }
+    },
+    "required": ["id", "name", "year", "color", "pantone_value"],
+    "additionalProperties": False
+}
